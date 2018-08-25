6cd895be9bdb78a9316dffb420aa22c118c4d6c2
Aleksandra Kurek <kurek.aleksandra@gmail.com>
2018-08-25 15:07:00 +0200

    Add file structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/layouts/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const rootEl = document.getElementById('root');
+
+let render = () => {
+  ReactDOM.render(<App/>, rootEl)
+}
+
+
+if(module.hot){
+  module.hot.accept('./app/layouts/App', ()=> {
+    setTimeout(render)
+  })
+}
+
+render();
+
 registerServiceWorker();
