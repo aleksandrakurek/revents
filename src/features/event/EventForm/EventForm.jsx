bbb6f05faa848be2b710cccf46080c2426cec6a1
Aleksandra Kurek <kurek.aleksandra@gmail.com>
2018-09-01 18:03:23 +0200

    Connect event form to the store

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -1,35 +1,31 @@
 import React, { Component } from 'react';
 import { Segment, Form, Button } from 'semantic-ui-react';
+import { connect } from 'react-redux';
 
+const mapState = (state, ownProps) => {
+  const eventId = ownProps.match.params.id;
 
-const emptyEvent = {
-  title: '',
-  date: '',
-  city: '',
-  venue: '',
-  hostedBy: '',
+  let event = {
+    title: '',
+    date: '',
+    city: '',
+    venue: '',
+    hostedBy: ''
+  }
+
+  if (eventId && state.events.length > 0) {
+    event = state.events.filter(event => event.id === eventId)[0];
+  }
+
+  return {
+    event
+  }
 }
 
 class EventForm extends Component {
   state = {
-    event: emptyEvent
+    event: Object.assign({}, this.props.event)
   };
-
-  componentDidMount() {
-    if (this.props.selectedEvent !== null) {
-      this.setState({
-        event: this.props.selectedEvent
-      })
-    }
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.selectedEvent !== this.props.selectedEvent) {
-      this.setState({
-        event: nextProps.selectedEvent || emptyEvent
-      })
-    }
-  }
 
   onFormSubmit = (evt) => {
     evt.preventDefault();
@@ -90,4 +86,4 @@ class EventForm extends Component {
 }
 
 
-export default EventForm;
+export default connect(mapState)(EventForm);
