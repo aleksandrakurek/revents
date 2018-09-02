04053eb26a1250742a75faa975d5f4e087a47d52
Aleksandra Kurek <kurek.aleksandra@gmail.com>
2018-09-02 16:08:37 +0200

    Add recent activity component

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -3,7 +3,8 @@ import { Grid } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { deleteEvent } from '../eventActions';
 import EventList from '../EventList/EventList';
-import LoadingComponent from '../../../app/layouts/LoadingComponent'
+import LoadingComponent from '../../../app/layouts/LoadingComponent';
+import EventActivity from '../EventActivity/EventActivity';
 
 const mapState = state => ({
   events: state.events,
@@ -21,14 +22,16 @@ class EventDashboard extends Component {
 
   render() {
     const { events, loading } = this.props;
-    if (loading) return <LoadingComponent inverted={true}/>
+    if (loading) return <LoadingComponent inverted={true}/>;
 
     return (
       <Grid>
         <Grid.Column width={10}>
           <EventList deleteEvent={this.handleDeleteEvent} events={events}/>
         </Grid.Column>
-        <Grid.Column width={6}/>
+        <Grid.Column width={6}>
+          <EventActivity />
+        </Grid.Column>
       </Grid>
     );
   }
