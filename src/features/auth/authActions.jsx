b8f1cfd13d52993b6b14a140c377f55504d6356f
Aleksandra Kurek <kurek.aleksandra@gmail.com>
2018-11-06 21:51:39 +0100

    Firebase auth

--- src/features/auth/authActions.jsx
+++ src/features/auth/authActions.jsx
@@ -2,19 +2,19 @@ import { SIGN_OUT_USER } from './authConstants';
 import { closeModal } from '../modals/modalActions';
 
 export const login = (creds) => {
-    return async (dispatch, getState, { getFirebase }) => {
-        const firebase = getFirebase();
-        try {
-            await firebase.auth().signInWithEmailAndPassword(creds.email, creds.password);
-            dispatch(closeModal())
-        } catch (error) {
-            console.log(error)
-        }
+  return async(dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase();
+    try {
+      await firebase.auth().signInWithEmailAndPassword(creds.email, creds.password);
+      dispatch(closeModal())
+    } catch (error) {
+      console.log(error)
     }
+  }
 };
 
 export const logout = () => {
-    return {
-        type: SIGN_OUT_USER
-    }
+  return {
+    type: SIGN_OUT_USER
+  }
 };
